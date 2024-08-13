8aa24799d3435bd7ba49d412e84cb1a7e23a6cd3
Camilla <milla_mendes83@hotmail.com>
2024-08-13 10:13:18 -0300

    Refatorando Produtos

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({children}) => {
+  const [carrinho, setCarrinho] = useState([]);
   return (
     <CarrinhoContext.Provider>
       {children}
